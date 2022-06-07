564a6573dc5448a425e98520e778b2b9880500ef
Egor Kharitonov <ega.khar@gmail.com>
2022-06-06 21:48:05 -0400

    edited contacts page

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const header = document.querySelector('#header')
 const ContactsButton = document.querySelector('#ContactsButton')
 const mainText = document.querySelector('#mainText')
 const mainContacts = document.querySelector('#mainContacts')
+const HomeButton = document.querySelector('#HomeButton')
+const mainContactstext = document.querySelector('#mainContactstext')
+const mainContactsTelegram = document.querySelector('#mainContactsTelegram')
+const mainContactsVk = document.querySelector('#mainContactsVk')
 
 var root = document.querySelector(':root')
 
@@ -23,9 +27,13 @@ darkthemebutton.onclick = function () {
         main.style.backgroundColor = "var(--main-color-dark)"
         header.style.backgroundColor = "var(--header-color-dark)"
         DarkButton.style.backgroundColor = "var(--header-color-dark)"
+        languagebutton.style.backgroundColor = "var(--header-color-dark)"
         container.style.backgroundColor = "var(--main-color-dark)"
+        container.style.boxShadow = "var(--shadow-color-dark)"
 
         body.style.color= 'var(--text-color-dark)'
+        mainContactsTelegram.style.color = 'var(--text-color-dark)'
+        mainContactsVk.style.color = 'var(--text-color-dark)'
         DarkButtonImage.src = "images/sun.png"
         DarkFlag = 0
 
@@ -34,10 +42,14 @@ darkthemebutton.onclick = function () {
         main.style.backgroundColor = "var(--main-color-white)"
         header.style.backgroundColor = "var(--header-color-white)"
         DarkButton.style.backgroundColor = "var(--header-color-white)"
+        languagebutton.style.backgroundColor = "var(--header-color-white)"
         container.style.backgroundColor = "var(--main-color-white)"
+        container.style.boxShadow = "var(--shadow-color-white)"
 
         DarkButtonImage.src = "images/moon.png"
         body.style.color= 'var(--text-color-white)';
+        mainContactsTelegram.style.color = 'var(--text-color-white)'
+        mainContactsVk.style.color = 'var(--text-color-white)'
         DarkFlag = 1
     }
 }
@@ -50,12 +62,18 @@ languagebutton.onclick = function () {
         helloworld.innerHTML = "Привет, мир!"
         titleName.innerHTML = "Харитонов Егор"
         LanguageButtonImage.src="images/us.png"
+        mainContactstext.innerHTML = "Контакты"
+        ContactsButton.innerHTML = "Контакты"
+        HomeButton.innerHTML = "Главная"
         LanguageFlag = 0
     } else if (LanguageFlag==0){
         MyName.innerHTML = "Egor Kharitonov"
         helloworld.innerHTML = "Hello, world!"
         titleName.innerHTML = "Kharitonov Egor"
         LanguageButtonImage.src="images/ru.png"
+        mainContactstext.innerHTML = "Contacts"
+        ContactsButton.innerHTML = "Contacts"
+        HomeButton.innerHTML = "Home"
         LanguageFlag = 1
     }
     
@@ -64,5 +82,9 @@ languagebutton.onclick = function () {
 ContactsButton.onclick = function () {
     mainText.style.display = "none"
     mainContacts.style.display = "block"
+}
 
+HomeButton.onclick = function () {
+    mainText.style.display = "block"
+    mainContacts.style.display = "none"
 }
